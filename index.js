f4b2bda3f307892fa8f31255949f0c127d89dd9c
Devolpment <devolpment@MacBook-Pro-2.local>
2023-10-17 19:52:53 -0700

    created a new branch

--- index.js
+++ index.js
@@ -1,43 +1,47 @@
 let display = document.getElementsByClassName("display");
 let buttons = document.getElementsByClassName("button");
-let FIRST_VALUE = "";
-let SECOND_VALUE = "";
+let FIRST_VALUE = "0";
+let SECOND_VALUE = "0";
 let OPERATOR = undefined;
 
 function userInput() {
   let input = this.dataset.value;
+  console.log(input);
   let className = this.className.split(" ").includes("number");
+  let emptyDisplay = display[0].textContent === "0" ? true : false;
+
   if (input === "clear") {
     display[0].textContent = 0;
-    FIRST_VALUE = "";
-    SECOND_VALUE = "";
+    FIRST_VALUE = 0;
+    SECOND_VALUE = 0;
     OPERATOR = undefined;
   }
 
-  /* work on positive negative */
   if (input === "positiveNegative") {
-    if (!FIRST_VALUE === "") {
-      FIRST_VALUE * -1;
+    if (OPERATOR) {
+      SECOND_VALUE[0] === "0"
+        ? (SECOND_VALUE[0] = "-")
+        : (SECOND_VALUE[0] = "0");
     } else {
-      SECOND_VALUE * -1;
+      FIRST_VALUE[0] === "0" ? (FIRST_VALUE[0] = "-") : (FIRST_VALUE[0] = "0");
     }
   }
 
   if (!OPERATOR && !isOperator(input) && className) {
-    if (display[0].textContent === "0") {
+    if (emptyDisplay) {
       display[0].textContent = input;
       FIRST_VALUE += input;
     } else {
       display[0].textContent += input;
       FIRST_VALUE += input;
     }
-  } else if (isOperator(input) && SECOND_VALUE === "") {
+  } else if (isOperator(input) && SECOND_VALUE === 0) {
     OPERATOR = input;
     display[0].textContent = 0;
   } else if (input === "equals") {
     display[0].textContent = result(FIRST_VALUE, SECOND_VALUE, OPERATOR);
   } else if (className) {
-    if (display[0].textContent === "0") {
+    if (emptyDisplay) {
       display[0].textContent = input;
       SECOND_VALUE += input;
     } else {
@@ -69,7 +73,7 @@ function result(firstValue, secondValue, operator) {
   } else if (operator === "minus") {
     return +firstValue - +secondValue;
   } else if (operator === "reminder") {
-    return +firstValue / +secondValue;
+    return +firstValue + +secondValue;
   }
 }
 

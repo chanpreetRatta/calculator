1666e2762538adf103cef42e25ded8c8eae4d8b1
Devolpment <devolpment@MacBook-Pro-2.local>
2023-12-10 11:08:38 -0800

    working calculator but reminder operator

--- index.js
+++ index.js
@@ -3,30 +3,88 @@ let buttons = document.querySelectorAll(".button");
 let clearButton = document.querySelector(`[data-value="clear"]`);
 let numbers = document.querySelectorAll(`[data-value="numbers"]`);
 let operators = document.querySelectorAll(`[data-value="operators"]`);
+let result = document.querySelector(`[data-value="result"]`);
+let positiveNegative = document.querySelector(
+  `[data-value="positiveNegative"]`
+);
 
 let equation = {
   firstNumber: "",
   secondNumber: "",
   operator: "",
+  result: "",
 };
+
+const calculator = (() => {
+  const add = (a, b) => parseInt(a) + parseInt(b);
+  const minus = (a, b) => parseInt(a) - parseInt(b);
+  const divide = (a, b) => parseInt(a) / parseInt(b);
+  const multiply = (a, b) => parseInt(a) * parseInt(b);
+
+  return { add, minus, divide, multiply };
+})();
 
 function updateTheDisplay() {
   display[0].removeChild(display[0].firstChild);
-  display[0].appendChild(document.createTextNode(value));
+
+  if (equation.result) {
+    display[0].appendChild(document.createTextNode(equation.result));
+    return;
+  }
+  let pointer = equation.operator
+    ? equation.secondNumber
+    : equation.firstNumber;
+  display[0].appendChild(document.createTextNode(pointer));
 }
 
 function clearDisplay() {
   display[0].removeChild(display[0].firstChild);
   display[0].appendChild(document.createTextNode("0"));
-  equation = {};
-}
-
-function getNumbers(event) {
-  updateTheDisplay();
+  equation = {
+    firstNumber: "",
+    secondNumber: "",
+    operator: "",
+  };
 }
 
 function getOperator(event) {
-  if (equation.firstNumber) equation.operator = event.target.innerHTML;
+  let signMeaning = {
+    "+": "add",
+    "-": "minus",
+    "÷": "divide",
+    x: "multiply",
+    "%": "reminder",
+  };
+
+  if (equation.firstNumber)
+    equation.operator = signMeaning[`${event.target.innerHTML}`];
+}
+
+function getNumbers(event) {
+  let value = event.target.innerHTML;
+  equation.operator
+    ? (equation.secondNumber = equation.secondNumber + value)
+    : (equation.firstNumber = equation.firstNumber + value);
+  updateTheDisplay();
+}
+
+function changeNegation() {
+  equation.operator
+    ? (equation.secondNumber = equation.secondNumber * -1)
+    : (equation.firstNumber = equation.firstNumber * -1);
+
+  updateTheDisplay();
+}
+
+function getResult() {
+  if (!(equation.firstNumber && equation.operator)) return;
+  let result = calculator[equation.operator](
+    equation.firstNumber,
+    equation.secondNumber
+  );
+
+  equation.result = result;
+  updateTheDisplay();
 }
 
 clearButton.addEventListener("click", clearDisplay);
@@ -34,3 +92,6 @@ numbers.forEach((number) => number.addEventListener("click", getNumbers));
 operators.forEach((operator) =>
   operator.addEventListener("click", getOperator)
 );
+
+positiveNegative.addEventListener("click", changeNegation);
+result.addEventListener("click", getResult);
